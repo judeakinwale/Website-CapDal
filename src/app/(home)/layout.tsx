b9813c1f0a544451bbb83509136f462d6488e305
judeakinwale <judeakinwale@gmail.com>
2026-09-22 18:45:06 +0100

    Minor Changes

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,13 +1,6 @@
 import HomeLayout from "@/components/layout/home/layout";
-import { defaultMetadata } from "@/components/metadata";
-import { Metadata } from "next";
 import { connection } from "next/server";
 import React from "react";
-
-export const metadata: Metadata = {
-  title: defaultMetadata.baseTitle,
-  description: defaultMetadata.description,
-};
 
 const Layout: React.FC<{ children: React.ReactNode }> = async ({
   children,
